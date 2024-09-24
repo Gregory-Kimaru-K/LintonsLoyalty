2000f2352d972f38bb2866ff8ba2bb0b75b9787e
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-09-24 10:30:33 +0300

    finished backend

--- lintons/lintons/settings.py
+++ lintons/lintons/settings.py
@@ -122,7 +122,7 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Africa/Nairobi'
 
-USE_I18N = True
+# USE_I18N = True
 
 USE_TZ = True
 
--- lintons/logUser/views.py
+++ lintons/logUser/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Loyalcustomers
 from django.http import JsonResponse
 import re
+from django.utils import timezone
+import pytz
+from datetime import timedelta
 
 # Create your views here.
 def signup_view(request):
@@ -13,9 +16,17 @@ def sign_up_cust(request):
         email = request.POST.get('email')
         phone = request.POST.get('phone')
         gender = request.POST.get('gender')
-        byear = request.POST.get('byear')
-        bmonth = request.POST.get('bmonth')
-        bday = request.POST.get('bday')
+
+        byear = request.POST.get('byear') or None
+        bmonth = request.POST.get('bmonth') or None
+        bday = request.POST.get('bday') or None
+
+        try:
+            byear = int(byear) if byear else None
+            bmonth = int(bmonth) if bmonth else None
+            bday = int(bday) if bday else None
+        except ValueError:
+            return JsonResponse({'msg': 'Invalid birth date format.', 'success': 'False'})  
 
         if re.match(r'^07\d{8}$', phone):
             phone = '254' + phone[1:]
@@ -39,10 +50,24 @@ def sign_up_cust(request):
             pass
 
         else:
-            return JsonResponse({'msg' : "Invalid phone number format."})
-        
+            return JsonResponse({'msg' : "Phone number format not supported.", 'success' : 'False'})
         
         if Loyalcustomers.objects.filter(phone=phone).exists():
-            return JsonResponse({'msg' : 'User with this number exists.'})
+            return JsonResponse({'msg' : 'User with this number exists.', 'success' : 'False'})
         
-        return JsonResponse({'msg' : 'Type shit!'})
+        else:
+            timecreated = timezone.now()
+            datecreated = timecreated + timedelta(hours=3)
+            Loyalcustomers.objects.create(
+                phone=phone,
+                customername=name,
+                gender=gender,
+                email=email,
+                datecreated=datecreated,
+                points=100,
+                lastupdated=datecreated,
+                birth_year=byear,
+                birth_month=bmonth,
+                birth_day=bday
+            )
+            return JsonResponse({'msg' : 'Type shit!', 'success' : 'True'})
